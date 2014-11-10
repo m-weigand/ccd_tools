0c46eaa453d436b03a1811c51a731167b486f418
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2014-11-10 09:31:51 +0100

    work on test cases for the normalization feature

--- src/dd_single/tests/NormMag/Readme.txt
+++ src/dd_single/tests/NormMag/Readme.txt
@@ -1,1 +1,12 @@
 Tests in this directory test the normalization feature
+
+Test Criteria
+=============
+
+* Input and output data (data.dat|data_format.dat) should be the same
+* The normalization factors (norm_factor.dat) should be as expected (given a
+  data set and a normalization magnitude)
+* Plots should contain the original data
+
+* Check for all possible input formats (rre_rmin, rmag_rpha, cre_rim, cmag_cpha)
+* Check for resistivity/conductivity model
---
+++ src/dd_single/tests/NormMag/test_norm_mag.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Run with
+
+nosetests test_dd_data.py -s -v
+"""
+import sys
+sys.path.append('..')
+from nose.tools import *
+import os
+import subprocess
+import shutil
+import numpy as np
+import lib_cc.main as cc
+
+
+def generate_data(directory, settings):
+    """Create data.dat, frequencies.dat in directory using the settings dict
+
+    settings : {'data_format': cre_cim|cmag_cpha|rmag_rpha|rre_rmim,
+                'rho0': float,
+                'frequencies': np.array()
+                }
+    """
+    cc_obj = cc.get('logrho0_m_logtau_c',
+                    {'frequencies': settings['frequencies']}
+                    )
+    pars = [np.log(100), 0.1, np.log(0.04), 0.6]
+    response = cc_obj.cc_rmag_rpha(pars)
+
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+
+    np.savetxt(directory + os.sep + 'frequencies.dat', settings['frequencies'])
+    np.savetxt(directory + os.sep + 'data.dat',
+               np.reshape(response.T, (1, settings['frequencies'].size * 2)))
+
+
+def generate_dd(directory, settings):
+    """Create a run_dd.sh batch file using the DD settings stored in the
+    settings dict:
+
+    settings : {'Nd': int,
+                'lam': float,
+                'data_format': sting,
+                'model': res|cond,
+                }
+    """
+    batchfile = directory + os.sep + 'run_dd.sh'
+    with open(batchfile, 'w') as fid:
+        fid.write('#!/bin/bash\n')
+        if settings['model'] == 'cond':
+            fid.write('DD_COND=1 ')
+        fid.write('dd_single.py -c 1')
+        fid.write(' -n {0}'.format(settings['Nd']))
+        fid.write(' --lambda {0}'.format(settings['lam']))
+        fid.write(' --norm_mag {0}'.format(settings['norm_mag']))
+        fid.write(' --data_format "{0}"'.format(settings['data_format']))
+        fid.write(' -o "results"')
+        fid.write('\n')
+
+
+def _check_io_datafiles(directory, dd_dir, format_orig):
+    datafile_orig = directory + '/data.dat'
+    datafile_fit = dd_dir + '/data.dat'
+    dataformatfile_fit = dd_dir + '/data_format.dat'
+
+    data_orig = np.loadtxt(datafile_orig)
+    data_fit = np.loadtxt(datafile_fit)
+    dataformat_fit = open(dataformatfile_fit, 'r').readline().strip()
+
+    data_diff = np.abs(data_orig - data_fit)
+    assert_equal(dataformat_fit, format_orig)
+    assert_greater(1e-5, data_diff.max())
+    if data_diff.max() > 1e-5:
+        print('Input/output data not equal')
+    else:
+        print('Input/output data equal')
+
+    if dataformat_fit == format_orig:
+        print('data_format checks out')
+    else:
+        print('data_format is wrong!', dataformat_fit, format_orig)
+
+
+def apply_checks(directory, settings_data, settings_dd):
+    """Check a given directory for correct results
+    """
+    dd_dir = directory + os.sep + 'results'
+
+    _check_io_datafiles(directory, dd_dir, settings_data['data_format'])
+
+
+def run_batch(directory):
+    print 'run_bash'
+    pwd = os.getcwd()
+    os.chdir(directory)
+    p = subprocess.Popen('bash run_dd.sh', shell=True,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    os.chdir(pwd)
+
+
+def test_generator():
+    """Generate the various test cases
+    """
+    for model in ('res', 'cond'):
+        for norm_mag in (0.1, 1, 10):
+            for data_format in ('rmag_rpha', 'rre_rmim', 'cmag_cpha',
+                                'cre_cim'):
+
+                directory = 'case_{0}_{1}_{2}'.format(model, norm_mag,
+                                                      data_format)
+                if os.path.isdir(directory):
+                    shutil.rmtree(directory)
+                settings_data = {'data_format': data_format,
+                                 'rho0': 100,
+                                 'frequencies': np.logspace(-3, 3, 20)
+                                 }
+                generate_data(directory, settings_data)
+
+                settings_dd = {'Nd': 20,
+                               'lam': 10,
+                               'data_format': data_format,
+                               'model': model,
+                               'norm_mag': norm_mag
+                               }
+                generate_dd(directory, settings_dd)
+                run_batch(directory)
+                # apply_checks(directory, settings_data, settings_dd)
+                yield apply_checks, directory, settings_data, settings_dd
+
+if __name__ == '__main__':
+    test_generator()
