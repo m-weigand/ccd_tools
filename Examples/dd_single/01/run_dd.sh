aa9dbaa2f8cec273e12e3e9ddfb0dc73ce29d32f
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2014-11-11 11:08:22 +0100

    Add conductivity inversion to example

--- Examples/dd_single/01/run_dd.sh
+++ Examples/dd_single/01/run_dd.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 test -d results && rm -r results
-dd_single.py -f frequencies.dat -d data.dat -n 20 -o results --tausel "data_ext" --nr_cores=1\
-    --plot --plot_lcurve -1
+dd_single.py -f frequencies.dat -d data.dat -n 20 -o results --nr_cores=1\
+     --plot --plot_lcurve -1
+
+test -d results_cond && rm -r results_cond
+DD_COND=1 dd_single.py -f frequencies.dat -d data.dat -n 20 -o results_cond --nr_cores=1\
+    --plot --plot_lcurve -1 --norm 1
