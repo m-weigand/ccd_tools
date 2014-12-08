da2ae5f4bd9edfc644d6a3cda65bb4db4e211759
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2014-12-08 10:27:36 +0100

    reduce size of examples

--- Examples/dd_space_time/02_synthetic_grid/run_dd.sh
+++ Examples/dd_space_time/02_synthetic_grid/run_dd.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# test -d results && rm -r results
-# for i in `seq 0 4`;
-# do
-#    mkdir -p results/tmp_ts_`printf %.4i $i`
-# done
-# echo 'done creating'
+# unpack data
+test -d data || tar xvjf data.tar.bz2
+
+# fit
+test -d results && rm -r results
 dd_space_time.py -f data/data/frequencies.dat\
     -d data/data/data_index.dat\
     --times data/data/times.dat\
