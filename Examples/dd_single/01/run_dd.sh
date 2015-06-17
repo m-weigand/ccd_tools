9dd7a9c6eb7e3277fe8440a1aa95197c0a2df0cd
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-06-17 21:34:26 +0200

    work on example

--- Examples/dd_single/01/run_dd.sh
+++ Examples/dd_single/01/run_dd.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # use the resistivity model for the Debye decomposition
 test -d results && rm -r results
-dd_single.py -f frequencies.dat -d data.dat -n 20 -o results --nr_cores=1\
-     --plot --plot_lcurve -1
+dd_single.py -f frequencies.dat -d data.dat\
+   	-n 20\
+   	-o results\
+   	--nr_cores=1\
+    --plot\
+   	--plot_lcurve -1
 
 # use the conductivity model for the Debye decomposition
-# notice that the conductivity model should always be normed to 10 S/m
+# notice that the conductivity model should always be normed (here: 10 S/m)
+# This ensures a more robust inversion
 test -d results_cond && rm -r results_cond
 DD_COND=1 dd_single.py -f frequencies.dat -d data.dat\
     -n 20\
