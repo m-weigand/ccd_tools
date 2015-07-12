3bb89e18117f029d6bb58e261bc22a828d9545a0
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-07-12 17:58:47 +0200

    modify example

--- Examples/dd_single/06_no_plot/run_dd.sh
+++ Examples/dd_single/06_no_plot/run_dd.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 test -d results && rm -r results
-python -m memory_profiler `which dd_single.py` -f frequencies.dat -d data.dat -n 20 -o results --tausel data_ext\
+dd_single.py -f frequencies.dat -d data.dat -n 20 -o results --tausel data_ext\
     --nr_cores=1\
-    --lambda 50\
-    --plot #\
-    #--plot_lcurve -1
+    --lambda 50
