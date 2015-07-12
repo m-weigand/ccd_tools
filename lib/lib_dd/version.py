4d4be4db7b9a801cdea0c0316c8a6aab0824975d
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-07-12 18:06:54 +0200

    fix version module in case we install using the meta-package "dd-tools"

--- lib/lib_dd/version.py
+++ lib/lib_dd/version.py
@@ -5,10 +5,20 @@ def _get_version_numbers():
     """Return a string containing the version numbers of geccoinv and
     dd_interface. The string is meant to be human readable.
     """
+    try:
+        geccoinv_version = pkg_resources.require('geccoinv')[0].version
+    except DistributionNotFound:
+        geccoinv_version = pkg_resources.require('dd-tools')[0].version
+
+    try:
+        dd_interfaces_version = pkg_resources.require('dd_interfaces')[0].version
+    except DistributionNotFound:
+        dd_interfaces_version = pkg_resources.require('dd-tools')[0].version
+
     versions = ''.join(('geccoinv version: ',
-                        pkg_resources.require('geccoinv')[0].version,
+                        geccoinv_version,
                         '\n',
                         'dd_interface version: ',
-                        pkg_resources.require('dd_interface')[0].version))
+                        dd_interfaces_version))
     return versions
 
