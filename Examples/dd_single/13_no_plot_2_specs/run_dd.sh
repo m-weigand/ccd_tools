3995b333463027977cba354ed675f725de73f413
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-07-12 22:44:29 +0200

    new example

---
+++ Examples/dd_single/13_no_plot_2_specs/run_dd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+test -d results && rm -r results
+dd_single.py -f frequencies.dat -d data.dat -n 20 -o results --tausel data_ext\
+    --nr_cores=1\
+    --lambda 50\
+	--output_format ascii_audit
+
