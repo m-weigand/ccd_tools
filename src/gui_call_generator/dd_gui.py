82699235a5d187ae18739133a7635434920fb1a6
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-07-12 22:45:07 +0200

    create file for gui

---
+++ src/gui_call_generator/dd_gui.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# *-* coding: utf-8 *-*
+import Tkinter as tk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.quitButton = tk.Button(self, text='Quit',
+            command=self.quit)
+        self.quitButton.grid()
+
+app = Application()
+app.master.title('Debye decomposition -- command call generator')
+app.mainloop()
