054032dffac2f43f2cce666b14f546344ee351d7
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-07-12 16:35:22 +0200

    create windows bat file for example

---
+++ Examples/dd_single/02/run_dd.bat
@@ -0,0 +1,8 @@
+REM extend the frequency range assymetrically: extend the low frequency by a
+REM  factor of 10, and the hight frequencies by a factor of 1000
+dd_single.py -f frequencies.dat -d data.dat^
+    -n 20^
+    -o results^
+    --tausel '10,1000'^
+    --nr_cores=1^
+    --plot
