b37845a1f0b28964aee1d476f6c57ca75bf7fb14
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-07-13 07:00:47 +0200

    add environment variable for windows example

--- Examples/dd_single/01/run_dd.bat
+++ Examples/dd_single/01/run_dd.bat
@@ -1,3 +1,4 @@
 REM This is a windows bat file, rename the ending to .bat, and execute
 
+set DD_COND=0
 dd_single.py -f frequencies.dat -d data.dat -n 20 -o results --tausel data_ext --nr_cores=1 --plot
