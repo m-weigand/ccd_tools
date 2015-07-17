36be0838dc8c89e999aa6b0d95048275564a4a99
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-07-17 14:50:47 +0200

    work on examples

--- Examples/dd_time/01/run_dd.sh
+++ Examples/dd_time/01/run_dd.sh
@@ -3,11 +3,11 @@
 # fit without any time regularization
 outdir="results_no_time"
 test -d ${outdir} && rm -r ${outdir}
-dd_time.py -f data/frequencies.dat --times data/times.dat\
+DD_STARTING_MODEL=3 dd_time.py -f data/frequencies.dat --times data/times.dat\
     -d data/data.dat\
     -o ${outdir}\
-    -c 1\
-    --plot
+    -c 1 --plot\
+	--f_lambda 1 --output_format ascii_audit
 
 exit
 # fit with time regularization in rho0
