0cd4f088302203906d5e33f1c9f7ae9ec45bc524
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-07-22 15:03:08 +0200

    add test case which should trigger an IOException if the output directory already exists.

---
+++ src/dd_single/tests/Functionality/DoNotOverwriteDirectory/run_dd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# this example uses two strategies to determine the starting model:
+
+test -d results && rm -r results
+DD_STARTING_MODEL=1 dd_single.py -f frequencies.dat -d data.dat -n 20 -o results --tausel data_ext\
+    --nr_cores=1\
+    --tausel data_ext\
+    --lambda 1.0\
+    -c 1 --max_it 20
+
+# try again, this should raise an Exception
+DD_STARTING_MODEL=1 dd_single.py -f frequencies.dat -d data.dat -n 20 -o results --tausel data_ext\
+    --nr_cores=1\
+    --tausel data_ext\
+    --lambda 1.0\
+    -c 1 --max_it 20 --plot_its
