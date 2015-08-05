c4d1d1bd83b238196870922d8951fe2829c68d84
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-08-05 17:42:43 +0200

    update copyright year

--- src/ddps/ddps.py
+++ src/ddps/ddps.py
@@ -1,7 +1,7 @@
 """
 ddpt.py is the post-processing tool for dd_single.py
 
-Copyright 2014 Maximilian Weigand
+Copyright 2014,2015 Maximilian Weigand
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
