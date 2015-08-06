8d5c79c66fb8026391831ad78ede6a295cfe33e5
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-08-06 15:02:00 +0200

    Windows run_dd.bat

---
+++ Examples/dd_time/LARGE/run_dd.bat
@@ -0,0 +1,7 @@
+dd_time.py -f frequencies.dat --times times.dat -d data.dat -o results_time_reg -c 1 --tausel data_ext^
+    --f_lambda 5^
+    --tm_i_lambda 0^
+    --trho0_lambda 0^
+	--output_format ascii
+
+ddpt.py -i results_time_reg/ --plot_stats
