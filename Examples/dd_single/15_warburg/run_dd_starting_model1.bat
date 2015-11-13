8572203bc0ec1d2d4d6a95a4389f98845543c0b6
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-11-13 11:06:27 +0100

    add Warburg example

---
+++ Examples/dd_single/15_warburg/run_dd_starting_model1.bat
@@ -0,0 +1,9 @@
+set DD_STARTING_MODEL=1
+
+dd_single.py -f frequencies.dat -d data.dat -n 20 -o results_1 --tausel data_ext^
+    --nr_cores=1^
+    --tausel data_ext^
+    --lambda 1.0^
+    --plot -c 1 --max_it 20 --plot_its
+
+pause
---
+++ Examples/dd_single/15_warburg/run_dd_starting_model3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+test -d results_3 && rm -r results_3
+DD_C=0.5 DD_STARTING_MODEL=3 dd_single.py -f frequencies.dat -d data.dat -n 20 -o results_3 --tausel data_ext\
+    --nr_cores=1\
+    --tausel data_ext\
+    --lambda 1.0\
+    --plot -c 1 --max_it 20 --plot_its
