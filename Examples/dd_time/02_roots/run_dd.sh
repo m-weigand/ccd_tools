45000ef9afcfd92f400ed8404ab4e8536377607e
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2016-08-10 15:02:53 +0200

    remove "-c" argument from dd_time examples: we only use one core

--- Examples/dd_time/02_roots/run_dd.sh
+++ Examples/dd_time/02_roots/run_dd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 test -d results_no_time_reg && rm -r results_no_time_reg
 dd_time.py -f frequencies.dat --times times.dat -d data.dat\
-    -o results_no_time_reg -c 1 --plot\
+    -o results_no_time_reg --plot\
     --f_lambda 5\
     --tm_i_lambda 0\
     --trho0_lambda 0 --output_format ascii
@@ -10,7 +10,7 @@ ddpt.py -i results_no_time_reg/ --plot_stats
 
 test -d results_time_reg && rm -r results_time_reg
 dd_time.py -f frequencies.dat --times times.dat -d data.dat\
-    -o results_time_reg -c 1 --plot\
+    -o results_time_reg --plot\
     --f_lambda 5\
     --tm_i_lambda 1\
     --trho0_lambda 10000 --output_format ascii
--- Examples/dd_time/06_simple/run_dd.sh
+++ Examples/dd_time/06_simple/run_dd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 test -d results_time_reg && rm -r results_time_reg
 dd_time.py -f frequencies.dat --times times.dat -d data.dat\
-    -o results_time_reg -c 1\
+    -o results_time_reg\
     --f_lambda 5\
     --tm_i_lambda 0\
     --trho0_lambda 0 --plot
