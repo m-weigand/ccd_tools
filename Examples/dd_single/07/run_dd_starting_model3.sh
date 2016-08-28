2db194f344f2bfa3d5d8855b1a4c0c0b09dd98b9
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2016-08-28 19:52:16 +0200

    change model

--- Examples/dd_single/07/run_dd_starting_model3.sh
+++ Examples/dd_single/07/run_dd_starting_model3.sh
@@ -5,4 +5,5 @@ DD_STARTING_MODEL=3 dd_single.py -f frequencies.dat -d data.dat -n 20 -o results
     --nr_cores=1\
     --tausel data_ext\
     --lambda 1.0\
-    --plot -c 1 --max_it 20 --plot_its
+    --plot -c 1 --max_it 20 --plot_its\
+	--output_format ascii
