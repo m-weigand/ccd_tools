22222834e0695e298a226179716af815e5aebff8
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2016-09-05 12:59:00 +0200

    move the ccd_single class to lib_dd.decomposition.ccd_single

--- lib/lib_dd/decomposition/ccd_single.py
+++ lib/lib_dd/decomposition/ccd_single.py
@@ -1,136 +1,131 @@
-import os
-import NDimInv
-import NDimInv.regs as RegFuncs
-import NDimInv.reg_pars as LamFuncs
-import gc
-import lib_dd.plot as lDDp
-import sip_formats.convert as sip_converter
-import lib_dd.conductivity.model as cond_model
-from lib_dd.models import ccd_res
+"""
 
+"""
+import ccd_single_stateless as decomp_single_sl
 import numpy as np
+from multiprocessing import Pool
+import lib_dd.interface as lDDi
 
 
-def _prepare_ND_object(fit_data):
-    # use conductivity or resistivity model?
-    if 'DD_COND' in os.environ and os.environ['DD_COND'] == '1':
-        # there is only one parameterisation: log10(sigma_i), log10(m)
-        model = cond_model.dd_conductivity(fit_data['inv_opts'])
-    else:
-        # there are multiple parameterisations available, use the log10 one
-        # model = lib_dd.main.get('log10rho0log10m', fit_data['inv_opts'])
-        if 'DD_C' in os.environ:
-            fit_data['inv_opts']['c'] = float(os.environ['DD_C'])
+class ccd_single(object):
+    """Cole-Cole decomposition object
+    """
+
+    def __init__(self, config):
+        self.config = config
+
+    def _filter_nan_values(self, frequencies, cr_spectrum):
+        """
+            Filter nan values along the frequency axis (we always ne part1 and
+            part2).
+
+            Return filtered frequencies, cr_spectrum
+        """
+
+        # check for NaN values
+        nan_indices = np.isnan(cr_spectrum)
+        nan_along_freq = np.any(nan_indices, axis=1)
+        to_delete = np.where(nan_along_freq)
+        frequencies_cropped = np.delete(frequencies, to_delete)
+        cr_spectrum_cropped = np.delete(cr_spectrum, to_delete, axis=0)
+        return frequencies_cropped, cr_spectrum_cropped
+
+    def _get_fit_datas(self, data):
+        """
+        Prepare data for fitting. Prepare a set of variables/objects for each
+        spectrum. Also filter nan values
+
+        Parameters
+        ----------
+        data : dict containing the keys 'frequencies', 'cr_data'
+        """
+        fit_datas = []
+
+        nr_of_spectra = len(data['cr_data'])
+        for i in range(0, nr_of_spectra):
+            fit_data = {}
+            fit_data['outdir'] = data['outdir']
+            # change file prefix for each spectrum
+            # at the moment we need a copy for this
+            frequencies_cropped, cr_data = self._filter_nan_values(
+                data['frequencies'], data['cr_data'][i])
+
+            fit_data['prep_opts'] = data['prep_opts']
+            fit_data['data'] = cr_data
+            fit_data['nr'] = i + 1
+            fit_data['nr_of_spectra'] = nr_of_spectra
+            fit_data['frequencies'] = frequencies_cropped
+
+            # inversion options are changed for each spectrum, so we have to
+            # copy it each time
+            inv_opts_i = data['inv_opts'].copy()
+            inv_opts_i['frequencies'] = frequencies_cropped
+            inv_opts_i['global_prefix'] = 'spec_{0:03}_'.format(i)
+            if('norm_factors' in data):
+                inv_opts_i['norm_factors'] = data['norm_factors'][i]
+            else:
+                inv_opts_i['norm_factors'] = None
+
+            fit_data['inv_opts'] = inv_opts_i
+
+            fit_datas.append(fit_data)
+
+        return fit_datas
+
+    def fit_data(self, data):
+        """This is the central fit function, which prepares the data, fits each
+        spectrum, plots (if requested), and then saves the results.
+        """
+        fit_datas = self._get_fit_datas(data)
+
+        # fit
+        if(data['prep_opts']['nr_cores'] == 1):
+            print('single processing')
+            # single processing
+            results = list(map(decomp_single_sl.fit_one_spectrum, fit_datas))
         else:
-            fit_data['inv_opts']['c'] = 1.0
-        # model = lib_cc2.decomposition_resistivity(fit_data['inv_opts'])
-        model = ccd_res.decomposition_resistivity(fit_data['inv_opts'])
-    ND = NDimInv.NDimInv(model, fit_data['inv_opts'])
-    ND.finalize_dimensions()
-    ND.Data.data_converter = sip_converter.convert
+            # multi processing
+            print('multi processing')
+            p = Pool(data['prep_opts']['nr_cores'])
+            results = p.map(decomp_single_sl.fit_one_spectrum, fit_datas)
 
-    # read in data
-    # print fit_data['data'], fit_data['prep_opts']['data_format']
-    ND.Data.add_data(
-        fit_data['data'],
-        fit_data['prep_opts']['data_format'],
-        extra=[]
-    )
+        self.results = results
+        self.data = data
+        # results now contains one or more ND objects
+        # iog.save_fit_results(data, results)
 
-    # now that we know the frequencies we can call the post_frequency
-    # handler for the model side
-    ND.update_model()
+    def get_data_dd_single(self, options, outdir):
+        """
+        Load frequencies and data and return a data dict
 
-    # add rms types
-    ND.RMS.add_rms('rms_re_im',
-                   [True, False],
-                   ['rms_real_parts', 'rms_imag_parts'])
+        Parameters
+        ----------
 
-    # use imaginary part for stopping criteria
-    ND.stop_rms_key = 'rms_re_im_noerr'
-    ND.stop_rms_index = 1
-
-    ND.set_custom_plot_func(lDDp.plot_iteration())
-
-    # rms value to optimize
-    optimize_rms_key = 'rms_re_im_noerr'
-    optimize_rms_index = 1  # imaginary part
-
-    # add a frequency regularization for the DD model
-    if(fit_data['prep_opts']['lambda'] is None):
-        lam_obj = LamFuncs.SearchLambda(LamFuncs.Lam0_Easylam())
-        lam_obj.rms_key = optimize_rms_key
-        lam_obj.rms_index = optimize_rms_index
-    else:
-        lam_obj = LamFuncs.FixedLambda(fit_data['prep_opts']['lambda'])
-
-    ND.Model.add_regularization(0,
-                                RegFuncs.SmoothingFirstOrder(
-                                    decouple=[0, ]),
-                                lam_obj
-                                )
-
-    # choose from a fixed set of step lengths
-    ND.Model.steplength_selector = NDimInv.main.SearchSteplengthParFit(
-        optimize_rms_key, optimize_rms_index)
-    return ND
+        options: cmd options
 
 
-# @profile
-def fit_one_spectrum(fit_data):
-    """
-    Fit one spectrum
-    """
-    print('Fitting spectrum {0} of {1}'.format(fit_data['nr'],
-                                               fit_data['nr_of_spectra']))
-    ND = _prepare_ND_object(fit_data)
+        Returns
+        -------
+        data: dict with entries "raw_data", "cr_data", "options", "inv_opts",
+              "prep_opts"
+        """
+        data, options = lDDi.load_frequencies_and_data(options)
 
-    # run the inversion
-    ND.run_inversion()
+        # we need list of spectra
+        size_y = int(data['raw_data'].shape[1] / 2)
+        cr_data = [x.reshape((size_y, 2), order='F') for x in data['raw_data']]
 
-    # extract the (only) iteration
-    final_iteration = ND.iterations[-1]
+        data['cr_data'] = cr_data
 
-    # renormalize data (we deal only with one spectrum here)
-    if(False and fit_data['inv_opts']['norm_factors'] is not None):
-        norm_fac = fit_data['inv_opts']['norm_factors']
+        # we distinguish two sets of options:
+        # prep_opts : all settings we need to prepare the inversion (i.e. set
+        #             regularization objects)
+        # inv_opts : options that are directly looped through to the NDimInv
+        # object
+        prep_opts, inv_opts = options.split_options()
 
-        # add normalization factor to the parameters
-        final_iteration.m[0] -= np.log10(norm_fac)
-        final_iteration.f = final_iteration.Model.f(final_iteration.m)
-        # data
-        # note: the normalization factor can be applied either to the
-        # magnitude, or to both real and imaginary parts!
-        final_iteration.Data.D /= norm_fac
-
-    call_fit_functions(fit_data, ND)
-
-    # invoke the garbage collection just to be sure
-    gc.collect()
-    return ND
-
-
-def call_fit_functions(fit_data, ND):
-    # run plot functions in output directory
-    pwd = os.getcwd()
-    os.chdir(fit_data['outdir'])
-    print('Changing to: {0}'.format(fit_data['outdir']))
-
-    if(fit_data['prep_opts']['plot']):
-        print('Plotting final iteration')
-        ND.iterations[-1].plot(
-            norm_factors=fit_data['inv_opts']['norm_factors'])
-        ND.iterations[-1].Model.obj.plot_stats(
-            '{0}'.format(fit_data['nr'])
-        )
-
-    if(fit_data['prep_opts']['plot_reg_strength']):
-        ND.iterations[-1].plot_reg_strengths()
-
-    if(fit_data['prep_opts']['plot_it_spectra']):
-        for it in ND.iterations:
-            it.plot()
-
-    if(fit_data['prep_opts']['plot_lambda'] is not None):
-        ND.iterations[fit_data['prep_opts']['plot_lambda']].plot_lcurve()
-    os.chdir(pwd)
+        data['outdir'] = outdir
+        data['options'] = options
+        data['prep_opts'] = prep_opts
+        data['inv_opts'] = inv_opts
+        return data
