4cc2eec19dbf269a3eec430800b8f1fd8f59822d
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2016-12-27 18:25:46 -0800

    fix cmd option in example

--- Examples/dd_time/01/run_dd_time_reg.sh
+++ Examples/dd_time/01/run_dd_time_reg.sh
@@ -6,6 +6,5 @@ test -d ${outdir} && rm -r ${outdir}
 dd_time.py -f data/frequencies.dat --times data/times.dat\
     -d data/data.dat\
     -o ${outdir}\
-    -c 1\
     --trho0_lambda 100\
     --plot
