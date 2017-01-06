2b57afdbe98fcd547cfe5f84d26c3db48fd4e08d
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-01-06 15:26:30 +0100

    update python example for using the ccd

--- Examples/python/01/ccd_python.py
+++ Examples/python/01/ccd_python.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
 # *-* coding: utf-8 *-*
-# simple example on using the ccd_single module directly from Python
+# simple example for using the ccd_single module directly with Python
 import lib_dd.decomposition.ccd_single as ccd_single
 import lib_dd.config.cfg_single as cfg_single
 import numpy as np
-
-# import os
-# import shutil
-# if os.path.isdir('results'):
-#     shutil.rmtree('results')
-# os.makedirs('results')
 
 frequencies = np.loadtxt('data/frequencies.dat')
 data = np.loadtxt('data/data.dat')
@@ -20,10 +14,18 @@ config['frequency_file'] = frequencies
 config['data_file'] = data
 config['fixed_lambda'] = 10
 
+# generate a ccd object
 ccd_obj = ccd_single.ccd_single(config)
+
+# commence with the actual fitting
 ccd_obj.fit_data()
 
+# extract the last iteration
 last_it = ccd_obj.results[0].iterations[-1]
+
 print(dir(last_it))
 print('fit parameters', last_it.m)
 print('stat_pars', last_it.stat_pars)
+
+# save to directory
+ccd_obj.save_to_directory()
