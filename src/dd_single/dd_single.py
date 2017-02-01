541acc102bdfad2a5b0a40a0cdb9d58b79e09f3c
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-02-01 14:17:10 +0100

    reorder imports

--- src/dd_single/dd_single.py
+++ src/dd_single/dd_single.py
@@ -21,15 +21,17 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 # from memory_profiler import *
 import logging
-logging.basicConfig(level=logging.INFO)
 import os
 import shutil
-from NDimInv.plot_helper import *
-import lib_dd.io.io_general as iog
+
 import lib_dd.config.cfg_single as cfg_single
 import lib_dd.interface as lDDi
+import lib_dd.io.io_general as iog
 # import lib_dd.plot as lDDp
 from lib_dd.decomposition.ccd_single import ccd_single
+from NDimInv.plot_helper import *
+
+logging.basicConfig(level=logging.INFO)
 
 
 # @profile
