ae1b1db3a44fabb26fef9de90d3269a785c2e776
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-02-01 14:17:41 +0100

    add comment and simplify code

--- lib/lib_dd/base_class.py
+++ lib/lib_dd/base_class.py
@@ -166,9 +166,12 @@ class starting_pars_3():
 
         # rho0 can be approximated by the low-frequency magnitude
         self.rho0 = np.sqrt(re[0] ** 2 + mim[0] ** 2)
+
+        # Check if the conductivity formulation was turned on using the
+        # environment variable DD_COND=1
         # the conductivity formulation uses sigma_\infty, i.e. the
         # high-frequency limit
-        if 'DD_COND' in os.environ and os.environ['DD_COND'] == '1':
+        if int(os.environ.get('DD_COND', 0)) == 1:
             self.rho0 = np.sqrt(re[-1] ** 2 + mim[-1] ** 2)
 
         # compute bins for each frequency decade
