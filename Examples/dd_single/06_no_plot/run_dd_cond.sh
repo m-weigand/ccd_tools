89be2ee237a3fa9cab42e1308e516cd255551ab5
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-09-13 14:32:16 +0200

    add a conductivity call to one example

--- Examples/dd_single/06_no_plot/run_dd_cond.sh
+++ Examples/dd_single/06_no_plot/run_dd_cond.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 test -d results && rm -r results
-dd_single.py -f frequencies.dat -d data.dat -n 20\
+DD_COND=1 dd_single.py -f frequencies.dat -d data.dat -n 20\
    	-o results\
    	--tausel data_ext\
     --nr_cores=1\
---
+++ Examples/dd_single/06_no_plot/run_dd_res.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+test -d results && rm -r results
+dd_single.py -f frequencies.dat -d data.dat -n 20\
+	-o results\
+   	--tausel data_ext\
+    --nr_cores=1\
+	--norm 10\
+    --lambda 50
+
