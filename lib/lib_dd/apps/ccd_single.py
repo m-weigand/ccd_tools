cfa9529501148551ef7d667b653e0f27187b9554
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-09-23 11:53:13 +0200

    work on notebook app

--- lib/lib_dd/apps/ccd_single.py
+++ lib/lib_dd/apps/ccd_single.py
@@ -1,21 +1,34 @@
 # jupyter notebook app for ccd_single
 import os
 import shutil
+
 import ipywidgets as widgets
 from IPython.core.display import display, HTML
 import numpy as np
+
+import NDimInv.plot_helper
+plt, mpl = NDimInv.plot_helper.setup()
 import lib_dd.decomposition.ccd_single as ccd_single
 import lib_dd.config.cfg_single as cfg_single
 
 
 class ccd_single_app(object):
     def __init__(self, frequency_file, data_file):
+        self.style = {'description_width': 'initial'}
         self.frequencies = np.loadtxt(frequency_file)
         self.data = np.loadtxt(data_file)
-        self.widgets = []
+        self.widgets = {}
+        # items to be arranged in a VBox later
+        self.items = []
+        # store containers for parameter groups here
+        self.containers = {}
         self.vbox = None
         self.print_data_summary()
         self._init_app_widgets()
+
+        # we generate links to the online documentation. To simplify things,
+        # use the url-prefix from here
+        self.url = 'https://m-weigand.github.io/ccd_tools/doc_ccd/'
 
     def print_data_summary(self):
         """Print a summary of the data, e.g., number of frequencies, min/max
@@ -43,8 +56,35 @@ class ccd_single_app(object):
         display(HTML('<h2>Data summary</h2>'))
         display(HTML('<br />'.join(summary)))
 
-    def _init_app_widgets(self):
-        style = {'description_width': 'initial'}
+    def _add_dformat_selection(self):
+        w_dformat = widgets.Dropdown(
+            options=[
+                'rmag_rpha',
+                'lnrmag_rpha',
+                'log10rmag_rpha',
+                'rmag_rpha',
+                'rre_rim',
+                'rre_rmim',
+                'cmag_cpha',
+                'cre_cim',
+                'cre_cmim',
+            ],
+            value='rre_rim',
+            description='Input data format:',
+            style=self.style,
+        )
+        h_dformat = widgets.HTML(
+            value='<a href="https://m-weigand.github.io/ccd_tools/doc_ccd/' +
+            'usage_and_implementation.html#" ' +
+            'target="_blank">Help</a>',
+        )
+        hb_dformat = widgets.HBox(
+            children=[w_dformat, h_dformat]
+        )
+        self.widgets['-01_data_format'] = w_dformat
+        self.items.append(hb_dformat)
+
+    def _add_condres_selection(self):
         w_condres = widgets.Dropdown(
             options={
                 'resistivity formulation': '0',
@@ -52,18 +92,282 @@ class ccd_single_app(object):
             },
             value='0',
             description='Type of kernel:',
-            style=style,
+            style=self.style,
         )
+        h_condres = widgets.HTML(
+            value='<a href="https://m-weigand.github.io/ccd_tools/doc_ccd/' +
+            'usage_and_implementation.html#" ' +
+            'target="_blank">Help</a>',
+        )
+        hb_condres = widgets.HBox(
+            children=[w_condres, h_condres]
+        )
+        self.widgets['type_formulation'] = w_condres
+        self.items.append(hb_condres)
+
+    def _add_flimits(self):
+        w_fmin = widgets.FloatText(
+            value=self.frequencies.min(),
+            description='Frequency min:',
+            style=self.style,
+        )
+        w_fmax = widgets.FloatText(
+            value=self.frequencies.max(),
+            description='Frequency max:',
+            style=self.style,
+        )
+
+        h_fminmax = widgets.HTML(
+            value='<a href="https://m-weigand.github.io/ccd_tools/doc_ccd/' +
+            'usage_and_implementation.html#" ' +
+            'target="_blank">Help</a>',
+        )
+        hb_fminmax = widgets.HBox(
+            children=[w_fmin, w_fmax, h_fminmax]
+        )
+        self.widgets['fmin'] = w_fmin
+        self.widgets['fmax'] = w_fmax
+        self.items.append(hb_fminmax)
+
+        def fminmax_change(change):
+            if change['name'] == 'value':
+                fmin = self.widgets['fmin'].value
+                fmax = self.widgets['fmax'].value
+
+                # hide all frequencies in fignore that are outside the range
+                fign = self.widgets['fignore']
+                indices = np.where(
+                    (fmin <= self.frequencies) &
+                    (self.frequencies <= fmax)
+                )[0]
+                for nr, wdg in enumerate(fign):
+                    if nr in indices:
+                        wdg.layout.visibility = 'visible'
+                    else:
+                        wdg.layout.visibility = 'hidden'
+
+        w_fmin.observe(fminmax_change)
+        w_fmax.observe(fminmax_change)
+
+    def _add_fignore(self):
+        f_ign = []
+        for i in range(0, self.frequencies.size):
+            wid = widgets.Checkbox(
+                value=True,
+                description='{0:.2f} Hz'.format(self.frequencies[i]),
+                disabled=False,
+                # width=50,
+            )
+            f_ign.append(wid)
+        box_layout = widgets.Layout(
+            width='100%',
+            flex_flow='row wrap',
+            display='inline-flex',
+        )
+        vb_fignore = widgets.VBox(
+            children=f_ign,
+            layout=box_layout,
+        )
+        accordion = widgets.Accordion(
+            children=[vb_fignore, ]
+        )
+        accordion.set_title(0, 'Ignore frequencies')
+        accordion.selected_index = None
+
+        self.widgets['fignore'] = f_ign
+        self.items.append(accordion)
+
+    def _add_max_its(self):
+        w_max_its = widgets.IntSlider(
+            value=20,
+            min=0,
+            max=40,
+            step=1,
+            description='Maximum nr of iterations:',
+            disabled=False,
+            continuous_update=False,
+            orientation='horizontal',
+            readout=True,
+            readout_format='d',
+            style=self.style,
+        )
+        h_max_its = widgets.HTML(
+            value='<a href="https://m-weigand.github.io/ccd_tools/doc_ccd/' +
+            'usage_and_implementation.html#" ' +
+            'target="_blank">Help</a>',
+        )
+        hb_max_its = widgets.HBox(
+            children=[w_max_its, h_max_its]
+        )
+        self.widgets['max_its'] = w_max_its
+        self.items.append(hb_max_its)
+
+    def _add_nr_terms(self):
+        w_nr_terms = widgets.IntText(
+            value=20,
+            description='Nr of terms per frequency decade:',
+            disabled=False,
+            style=self.style,
+        )
+        h_nr_terms = widgets.HTML(
+            value='<a href="https://m-weigand.github.io/ccd_tools/doc_ccd/' +
+            'usage_and_implementation.html#" ' +
+            'target="_blank">Help</a>',
+        )
+        # h_nr_terms = widgets.HTML(
+        #     value='<iframe src="https://m-weigand.github.io/ccd_tools/' +
+        #     'doc_ccd/"></iframe>',
+        # )
+        hb_nr_terms = widgets.HBox(
+            children=[w_nr_terms, h_nr_terms]
+        )
+        self.widgets['nr_terms'] = w_nr_terms
+        self.items.append(hb_nr_terms)
+
+    def _add_tausel(self):
+        w_tausel = widgets.Dropdown(
+            options=[
+                'data_ext',
+                'data',
+                'individual',
+            ],
+            value='data_ext',
+            description='Range of tau values:',
+            style=self.style,
+        )
+        w_tausel_ind_left = widgets.IntText(
+            value=100,
+            description='left:',
+            disabled=True,
+            visible=False,
+        )
+        w_tausel_ind_left.layout.visibility = 'hidden'
+        w_tausel_ind_right = widgets.IntText(
+            value=100,
+            description='right:',
+            disabled=True,
+            visible=False,
+        )
+        w_tausel_ind_right.layout.visibility = 'hidden'
+
+        h_tausel = widgets.HTML(
+            value='<a href="https://m-weigand.github.io/ccd_tools/doc_ccd/' +
+            'usage_and_implementation.html#" ' +
+            'target="_blank">Help</a>',
+        )
+
+        hb_tausel = widgets.HBox(
+            children=[
+                w_tausel, w_tausel_ind_left, w_tausel_ind_right, h_tausel
+            ]
+        )
+        self.widgets['tausel'] = w_tausel
+        self.widgets['tausel_left'] = w_tausel_ind_left
+        self.widgets['tausel_right'] = w_tausel_ind_right
+        self.items.append(hb_tausel)
+
+        def tausel_change(change):
+            if change['name'] == 'value':
+                if change['new'] == 'individual':
+                    w_tausel_ind_left.layout.visibility = 'visible'
+                    w_tausel_ind_right.layout.visibility = 'visible'
+                else:
+                    w_tausel_ind_left.layout.visibility = 'hidden'
+                    w_tausel_ind_right.layout.visibility = 'hidden'
+
+        w_tausel.observe(tausel_change)
+
+    def _add_plot_options(self):
+        w_generate_plot = widgets.Checkbox(
+            value=True,
+            description='Generate plot',
+            disabled=False
+        )
+        w_generate_it_plots = widgets.Checkbox(
+            value=False,
+            description='Generate plots for all iterations',
+            disabled=True,
+        )
+        w_generate_lcurve = widgets.Checkbox(
+            value=False,
+            description='Plot lcurve',
+            disabled=False
+        )
+        w_generate_reg_strength = widgets.Checkbox(
+            value=False,
+            description='Plot regularization strength ',
+            disabled=False
+        )
+        w_generate_output = widgets.Checkbox(
+            value=True,
+            description='Generate output for download',
+            disabled=False
+        )
+        box_layout = widgets.Layout(
+            width='100%',
+            flex_flow='row wrap',
+            display='inline-flex',
+            border='3px solid black',
+        )
+        hb_output = widgets.VBox(
+            [
+                w_generate_plot,
+                w_generate_it_plots,
+                w_generate_lcurve,
+                w_generate_reg_strength,
+                w_generate_output,
+            ],
+            layout=box_layout,
+        )
+        self.widgets['generate_plot'] = w_generate_plot
+        self.widgets['generate_output'] = w_generate_output
+        self.widgets['generate_it_plots'] = w_generate_it_plots
+
+        self.items.append(hb_output)
+
+    def _add_data_weighting(self):
+        w_data_weighting = widgets.Dropdown(
+            options=[
+                're_vs_im',
+                'avg_im',
+                'avg_im_err',
+                'one',
+                'all_to_one',
+                'rel_abs_error',
+            ],
+            value='re_vs_im',
+            description='Data weighting scheme:',
+            style=self.style,
+        )
+        h_data_weighting = widgets.HTML(
+            value='<a href="https://m-weigand.github.io/ccd_tools/doc_ccd/' +
+            'usage_and_implementation.html#data_weighting" ' +
+            'target="_blank">Help</a>',
+        )
+        hb_data_weighting = widgets.HBox(
+            children=[w_data_weighting, h_data_weighting]
+        )
+        self.widgets['data_weighting'] = w_data_weighting
+        self.items.append(hb_data_weighting)
+
+    def _add_lambda(self):
         w_lambda = widgets.IntText(
             value=10,
             description='Lambda',
             disabled=False,
         )
-        w_run = widgets.Button(
-            description='Run CCD',
-            disabled=False,
-            tooltip='Run the Cole-Cole decomposition',
+        h_lambda = widgets.HTML(
+            value='<a href="https://m-weigand.github.io/ccd_tools/doc_ccd/' +
+            'usage_and_implementation.html#data_weighting" ' +
+            'target="_blank">Help</a>',
         )
+        hb_lambda = widgets.HBox(
+            children=[w_lambda, h_lambda]
+        )
+        self.widgets['lambda'] = w_lambda
+        self.items.append(hb_lambda)
+
+    def _add_c(self):
         w_c = widgets.FloatSlider(
             value=0.5,
             min=0.1,
@@ -76,12 +380,23 @@ class ccd_single_app(object):
             readout=True,
             readout_format='.1f',
         )
+        h_c = widgets.HTML(
+            value='<a href="https://m-weigand.github.io/ccd_tools/doc_ccd/' +
+            'usage_and_implementation.html#data_weighting" ' +
+            'target="_blank">Help</a>',
+        )
+        hb_c = widgets.HBox(
+            children=[w_c, h_c]
+        )
+        self.widgets['c'] = w_c
+        self.items.append(hb_c)
 
+    def _add_normalization(self):
         w_normalization = widgets.Checkbox(
             value=False,
             description='Activate normalization',
             disabled=False,
-            style=style,
+            style=self.style,
         )
         help_normalization = widgets.HTML(
             value='<a href="https://m-weigand.github.io/ccd_tools/doc_ccd/' +
@@ -93,41 +408,16 @@ class ccd_single_app(object):
             step=1,
             description='Normalization value:',
             disabled=True,
-            style=style,
+            style=self.style,
         )
-        hbox_norm = widgets.HBox(
+        hb_norm = widgets.HBox(
             children=[
                 w_normalization,
                 w_norm_value,
                 help_normalization,
             ],
         )
-
-        # output options
-        w_generate_plot = widgets.Checkbox(
-            value=True,
-            description='Generate plot',
-            disabled=False
-        )
-        w_generate_it_plots = widgets.Checkbox(
-            value=False,
-            description='Generate plots for all iterations',
-            disabled=True,
-        )
-        w_generate_output = widgets.Checkbox(
-            value=True,
-            description='Generate output for download',
-            disabled=False
-        )
-        hbox_output = widgets.HBox(
-            [
-                w_generate_plot,
-                w_generate_it_plots,
-                w_generate_output,
-            ]
-        )
-
-        w_run.on_click(self.run_ccd)
+        self.containers['hb_norm'] = hb_norm
 
         def use_norm_change(change):
             if change['name'] == 'value':
@@ -137,60 +427,103 @@ class ccd_single_app(object):
                     w_norm_value.disabled = True
 
         w_normalization.observe(use_norm_change)
-        self.widgets = {
-            '00_type_formulation': w_condres,
-            '01_lambda': w_lambda,
-            '02_c_selection': w_c,
-            '03_use_normalization': w_normalization,
-            '04_norm_value': w_norm_value,
-            '05_help_normalization': help_normalization,
-            # output settings
-            '80_generate_plot': w_generate_plot,
-            '81_generate_output': w_generate_output,
-            '82_generate_it_plots': w_generate_it_plots,
-            # run button
-            '99_run': w_run,
-        }
+
+        self.widgets['use_norm'] = w_normalization
+        self.widgets['norm_value'] = w_norm_value
+        self.items.append(hb_norm)
+
+    def _add_advanced_toggle(self):
+        w_advanced = widgets.ToggleButton(
+            value=True,
+            description='Show advanced options',
+            disabled=False,
+            button_style='',
+            tooltip='',
+            icon='check'
+        )
+
+        def toggle_advanced(change):
+            if change['name'] == 'value':
+                if change['new'] is True:
+                    # advanced mode
+                    self.containers['hb_norm'].layout.visibility = 'visible'
+                else:
+                    # simple mode
+                    self.containers['hb_norm'].layout.visibility = 'hidden'
+
+        w_advanced.observe(toggle_advanced)
+        self.items.append(w_advanced)
+
+    def _init_app_widgets(self):
+        self._add_advanced_toggle()
+        self._add_dformat_selection()
+        self._add_condres_selection()
+        self._add_lambda()
+        self._add_c()
+        self._add_flimits()
+        self._add_fignore()
+        self._add_max_its()
+        self._add_nr_terms()
+        self._add_tausel()
+        self._add_data_weighting()
+        self._add_normalization()
+        self._add_plot_options()
+
+        w_run = widgets.Button(
+            description='Run CCD',
+            disabled=False,
+            tooltip='Run the Cole-Cole decomposition',
+            layout=widgets.Layout(width='50%', height='80px')
+        )
+        w_run.on_click(self.run_ccd)
+        hb_run = widgets.HBox(
+            children=[w_run, ],
+            layout=widgets.Layout(
+                width='100%',
+                align_items='center'
+            ),
+        )
+
+        self.widgets.update({
+            'run': w_run,
+        })
 
         header = widgets.HTML(
             value='<h3>Settings</h3>'
         )
 
-        self.vbox = widgets.VBox([
-            header,
-            w_condres,
-            w_lambda,
-            w_c,
-            hbox_norm,
-            hbox_output,
-            w_run,
+        self.items.insert(0, header)
+        self.items += [
+            hb_run,
         ]
-            # [self.widgets[key] for key in sorted(self.widgets.keys())]
-        )
+
+        self.vbox = widgets.VBox(self.items)
 
     def show(self):
         display(self.vbox)
         display(HTML('<hr />'))
 
     def run_ccd(self, button):
+        """Based on the GUI input, generate a configuration for the CCD and run
+        it
+        """
         print('running CCD')
-        print('lambda', self.widgets['01_lambda'].value)
         # set environment variables
-        os.environ['DD_COND'] = self.widgets['00_type_formulation'].value
+        os.environ['DD_COND'] = self.widgets['type_formulation'].value
         os.environ['DD_C'] = '{0:.2f}'.format(
-            self.widgets['02_c_selection'].value
+            self.widgets['c'].value
         )
 
         # set options using this dict-like object
         config = cfg_single.cfg_single()
         config['frequency_file'] = self.frequencies
         config['data_file'] = self.data
-        config['fixed_lambda'] = int(self.widgets['01_lambda'].value)
+        config['fixed_lambda'] = int(self.widgets['lambda'].value)
 
-        config['plot_it_spectra'] = self.widgets['82_generate_it_plots'].value
+        config['plot_it_spectra'] = self.widgets['generate_it_plots'].value
 
-        if self.widgets['03_use_normalization'].value is True:
-            config['norm'] = self.widgets['04_norm_value'].value
+        if self.widgets['use_norm'].value is True:
+            config['norm'] = self.widgets['norm_value'].value
 
         # generate a ccd object
         ccd_obj = ccd_single.ccd_single(config)
@@ -200,12 +533,14 @@ class ccd_single_app(object):
 
         # extract the last iteration
         last_it = ccd_obj.results[0].iterations[-1]
-        if self.widgets['80_generate_plot'].value is True:
+        if self.widgets['generate_plot'].value is True:
             print('plotting ... this may take a while')
+            # don't show in notebook
+            plt.ioff()
             _ = last_it.plot()
             _
 
-        if self.widgets['81_generate_output'].value is True:
+        if self.widgets['generate_output'].value is True:
             outdir = 'output'
             if os.path.isdir(outdir):
                 shutil.rmtree(outdir)
