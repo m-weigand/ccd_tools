76ffb833712210134e7a309ddd61cdf5bbfc7377
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-09-23 11:52:35 +0200

    switch example from dd_single.py to ccd_single command

--- Examples/dd_single/13_no_plot_3_specs/run_dd.bat
+++ Examples/dd_single/13_no_plot_3_specs/run_dd.bat
@@ -1,4 +1,4 @@
-dd_single.py -f frequencies.dat -d data.dat -n 20 -o results --tausel data_ext^
+ccd_single -f frequencies.dat -d data.dat -n 20 -o results --tausel data_ext^
     --nr_cores=1^
     --lambda 50^
 	--output_format ascii_audit
