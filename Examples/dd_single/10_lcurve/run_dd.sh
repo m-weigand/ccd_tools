e9cb6e4772b8d1f885ca7993251aaa622fcc1390
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-09-24 14:45:03 +0200

    convert dd_single.py to ccd_single in example

--- Examples/dd_single/10_lcurve/run_dd.sh
+++ Examples/dd_single/10_lcurve/run_dd.sh
@@ -3,7 +3,8 @@
 export DD_USE_LATEX=0
 
 test -d results && rm -r results
-DD_STARTING_MODEL=3 dd_single.py -f frequencies.dat -d data.dat -n 20 -o results --tausel data_ext\
+DD_STARTING_MODEL=3 ccd_single \
+	-f frequencies.dat -d data.dat -n 20 -o results --tausel data_ext\
     --nr_cores=1\
     --tausel data_ext\
     --lambda 1.0\
