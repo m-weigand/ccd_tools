fb24f82682d13b5fedebdb4a74adedea8d5ce63d
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-09-25 18:59:29 +0200

    increase version to 0.8.11

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import glob
 from setuptools import setup
 
 version_short = '0.8'
-version_long = '0.8.10'
+version_long = '0.8.11'
 
 # generate entry points
 entry_points = {'console_scripts': []}
