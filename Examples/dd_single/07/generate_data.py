5b8c22d3445da3b2dd86ec5ed6cd0d1d7da1ce42
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-11-21 17:27:18 +0100

    fix example nr 7

--- Examples/dd_single/07/generate_data.py
+++ Examples/dd_single/07/generate_data.py
@@ -1,19 +1,47 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """
 Generate sample SIP-Spectrum
 """
 import numpy as np
-import lib_dd.colecole as CC
 
-frequencies = np.logspace(-3, 3, 25)
+import sip_models.res.cc as cc_res
+# import lib_dd.colecole as CC
+
+frequencies = np.logspace(-3, 3, 30)
 fin = np.hstack((frequencies, frequencies))
 
 # rho0 m1 tau1 c1 m2 tau2 c2
-cc_pars = [np.log(50), 0.05, np.log(40), 0.6,
-           0.15, np.log(0.0001), 0.6]
+cc_pars = [
+    np.log(50),
+    0.05,
+    np.log(40),
+    0.6,
+    0.15,
+    np.log(0.0001),
+    0.6
+]
+cc_pars = [
+    # rho0
+    50,
+    # m
+    0.05,
+    0.15,
+    # tau
+    40,
+    0.01,
+    # c
+    0.6,
+    0.9
+]
 
-magpha = CC.cole_log(fin, cc_pars).flatten()[np.newaxis, :]
-magpha[0, 0:magpha.size / 2] = np.exp(magpha[0, 0:magpha.size / 2])
+obj = cc_res.cc(frequencies)
+response = obj.response(cc_pars)
+print(response.rmag_rpha)
 
-np.savetxt('data.dat', magpha)
+# magpha = CC.cole_log(fin, cc_pars).flatten()[np.newaxis, :]
+# magpha[0, 0:magpha.size / 2] = np.exp(magpha[0, 0:magpha.size / 2])
+
+np.savetxt(
+    'data.dat',
+    response.rmag_rpha.reshape((1, 2 * frequencies.size), order='F'))
 np.savetxt('frequencies.dat', frequencies)
