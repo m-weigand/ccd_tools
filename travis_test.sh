73d557288fceb45b16236fd70dcfab9a5d7b042b
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-02-06 14:35:13 +0100

    actually call the available tests in travis

--- travis_test.sh
+++ travis_test.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 
 # run tests
-cd tests/
-nosetests -s -v
-cd ..
+echo "Entering test/ directory and running nosetests -s -v:"
+cd tests/ccd_single
 
+	cd ColeCole1Term
+	nosetests -s -v
+	cd ..
+
+	cd Fitquality/
+	nosetests -s -v
+	cd ..
+
+	cd Functionality/StartingModels
+	nosetests -s -v
+	cd ../..
+
+	cd NoCrashTests
+	nosetests -s -v
+	cd ..
+
+	cd NormMag
+	nosetests -s -v
+	cd ..
+
+cd ../../
+
+echo "Build documentation:"
 # build documentation
 cd docs/doc
 make clean
