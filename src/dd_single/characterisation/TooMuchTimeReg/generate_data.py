2edd44b3fb7a5ba6ee2e82952e95bad172085deb
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2019-09-24 17:24:15 +0200

    fix an issue with old crlab imports

--- src/dd_single/characterisation/TooMuchTimeReg/generate_data.py
+++ src/dd_single/characterisation/TooMuchTimeReg/generate_data.py
@@ -4,10 +4,10 @@ Generate sample SIP-Spectra for 9 time steps, decreasing tau linearly
 """
 import os
 import numpy as np
-import crlab_py.colecole as CC
+# import crlab_py.colecole as CC
+from sip_models.res.cc import cc as cc_res
 
 frequencies = np.logspace(-2, 4, 20)
-fin = np.hstack((frequencies, frequencies))
 
 # generate CC parameters
 np.random.seed(5)
@@ -15,8 +15,8 @@ rho0 = 50
 
 m = np.linspace(0.01, 0.15, 20) + np.random.uniform(0, 0.01, size=20)
 
-#m = np.logspace(-2, -1, 3)
-#tau = np.logspace(np.log10(0.004), np.log10(0.4), 20)
+# m = np.logspace(-2, -1, 3)
+# tau = np.logspace(np.log10(0.004), np.log10(0.4), 20)
 tau = 0.004
 # note: we keep c constant
 c = 0.6
@@ -29,18 +29,24 @@ os.chdir(basedir)
 cr_data = []
 cc_list = []
 for timestep in range(0, 20):
-    cc_pars = [np.log(rho0), m[timestep], np.log(tau), c]
+    cc_pars = [rho0, m[timestep], tau, c]
     cc_list.append(cc_pars)
 
-    magpha = CC.cole_log(fin, cc_pars).flatten()[np.newaxis, :]
-    magpha[0, 0:magpha.size / 2] = np.exp(magpha[0, 0:magpha.size / 2])
+    ccobj = cc_res(frequencies=frequencies)
+    response = ccobj.response(cc_pars)
+    rmagpha = response.rmag_rpha
+    # convert rmag to exp
+    rmagpha[:, 0] = np.exp(rmagpha[:, 0])
 
-    cr_data.append(magpha)
+    # magpha = CC.cole_log(fin, cc_pars).flatten()[np.newaxis, :]
+    # magpha[0, 0:magpha.size / 2] = np.exp(magpha[0, 0:magpha.size / 2])
+
+    cr_data.append(rmagpha.flatten(order='F'))
 cr_data = np.array(cr_data).squeeze()
 
 np.savetxt('data.dat', cr_data)
 np.savetxt('frequencies.dat', frequencies)
-#times = range(0, 10) + range(20, 30)
+# times = range(0, 10) + range(20, 30)
 times = range(0, 20)
 np.savetxt('times.dat', times)
 np.savetxt('cc_pars.dat', np.array(cc_list))
